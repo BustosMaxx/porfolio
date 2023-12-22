4fb91b785cdfea722761288f8ec8c726cfb29149
Bustos Maximilano <bustoss.maxi@gmail.com>
2023-12-22 08:44:11 -0300

    Setup del proyecto -Heart attack analysis-. Utilicé cookiecutter

---
+++ heart_attack_analysis/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='heart_attack_analysis',
+    packages=find_packages(),
+    description='Analisis descriptivo y predicitvo.',
+    author='Your name (or your organization/company/team)',
+    version='0.0.1'
+)
